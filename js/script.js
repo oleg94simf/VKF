06031274906cbeb272e9ec7e78761a6afba0cced
Oleh <70809643+oleg94simf@users.noreply.github.com>
2021-11-04 01:46:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+
+$(document).ready(function () {
+	$("#phone").mask("+38 (999) -999-99-99", {
+		completed: function () {
+			$("#phone ").addClass('valid');
+
+		}
+	});
+	let poreerwe = document.querySelector('#phone');
+	poreerwe.addEventListener('keydown', function (event) {
+		if (event.code == 'Backspace' || event.code == 'Escape') {
+			$("#phone").removeClass('valid');
+
+		}
+		console.log(event.code);
+	});
+
+})
